2e956bd19b77d578fbc3ccefe90fad7bc2275105
ernilson <ernilson25@gmail.com>
2025-10-29 10:28:08 -0300

    implementado o Webhook

--- pedidos/src/main/java/br/com/Icompras/pedidos/service/PedidoService.java
+++ pedidos/src/main/java/br/com/Icompras/pedidos/service/PedidoService.java
@@ -2,13 +2,18 @@ package br.com.Icompras.pedidos.service;
 
 import br.com.Icompras.pedidos.client.ServicoBancarioClient;
 import br.com.Icompras.pedidos.model.Pedido;
+import br.com.Icompras.pedidos.model.enums.StatusPedido;
 import br.com.Icompras.pedidos.repository.ItemPedidoRepository;
 import br.com.Icompras.pedidos.repository.PedidoRepository;
 import br.com.Icompras.pedidos.validator.Pedidovalidator;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PedidoService {
@@ -35,5 +40,25 @@ public Pedido criarPedido(Pedido pedido){
     private void realizarPersistencia(Pedido pedido) {
         repository.save(pedido);
         itemPedidoRepository.saveAll(pedido.getItens());
+    }
+
+    public void atualizarStatusPagamento(Long codigoPedido, String chavePagamento, boolean sucesso, String observacoes) {
+
+        Optional<Pedido> pedidoEncontrado = repository.findByCodigoAndChavePagamento(codigoPedido, chavePagamento);
+
+        if (pedidoEncontrado.isEmpty()){
+            var msg = String.format("Pedido não encontrado para o código %d e chave pgmto %s",
+                    codigoPedido, chavePagamento);
+            log.error(msg);
+            return;
+        }
+        Pedido pedido = pedidoEncontrado.get();
+        if (sucesso){
+            pedido.setStatus(StatusPedido.PAGO);
+        }else {
+            pedido.setStatus(StatusPedido.ERRO_PAGAMENTO);
+            pedido.setObservacoes(observacoes);
+        }
+        repository.save(pedido);
     }
 }
